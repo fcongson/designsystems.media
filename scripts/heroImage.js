892839706902976cdda7c72eccec63c4a9859555
Josh Harwood <josh@noworries.nz>
2023-10-17 16:15:54 +1300

    script working great

--- scripts/heroImage.js
+++ scripts/heroImage.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 const sharp = require("sharp");
-
 const sourceDir = "../src/content/media"; // Path to the directory with subdirectories
 const outputImage = "../public/heroImage.jpg"; // Path for the final output image
+const numImagesToSelect = 30; // Number of images to select and process
 
 // Function to create a black background image
 function createBlackBackground(outputWidth, outputHeight) {
@@ -17,13 +17,8 @@ function createBlackBackground(outputWidth, outputHeight) {
   });
 }
 
-// Function to resize and save images
-async function resizeAndSaveImages(
-  selectedImages,
-  imageWidth,
-  imageHeight,
-  tempDir
-) {
+// Function to resize and save images while maintaining the aspect ratio
+async function resizeAndSaveImages(selectedImages, imageWidth, tempDir) {
   for (let index = 0; index < selectedImages.length; index++) {
     const image = selectedImages[index];
 
@@ -31,10 +26,17 @@ async function resizeAndSaveImages(
       console.log(`Image does not exist: ${image}`);
       continue; // Skip this image and move to the next one
     }
+
+    const imageInfo = await sharp(image).metadata();
+    const aspectRatio = imageInfo.width / imageInfo.height;
+
+    const newHeight = Math.round(imageWidth / aspectRatio);
+
     const fileName = `image_${index}.jpg`;
-    console.log(`Copying ${image} to ${path.join(tempDir, fileName)}`);
+    console.log(`Copying and resizing ${image}`);
+
     await sharp(image)
-      .resize(imageWidth, imageHeight)
+      .resize(imageWidth, newHeight) // Resize while maintaining aspect ratio
       .toFile(path.join(tempDir, fileName));
   }
 }
@@ -67,9 +69,9 @@ function compositeImages(
   }).composite(compositeImages);
 }
 
-// Step 1: Read images from different folders
+// Step 1: Read all potential image files from different folders
 const folders = fs.readdirSync(sourceDir);
-const selectedImages = [];
+const potentialImages = [];
 
 folders.forEach((folder) => {
   const folderPath = `${sourceDir}/${folder}`;
@@ -84,18 +86,30 @@ folders.forEach((folder) => {
     );
 
     if (imageFiles.length > 0) {
-      // Assuming "hqdefault.jpg" exists in each subdirectory
-      const selectedImage = path.join(folderPath, imageFiles[0]);
-      selectedImages.push(selectedImage);
+      // Assuming "maxresdefault.jpg" exists in each subdirectory
+      const potentialImage = path.join(folderPath, imageFiles[0]);
+      potentialImages.push(potentialImage);
     }
   }
 });
 
+// Shuffle the potential images randomly
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+shuffleArray(potentialImages);
+
+// Select the first 30 images from the shuffled array
+const selectedImages = potentialImages.slice(0, numImagesToSelect);
+
 // Set the output dimensions
-const numRows = 5;
-const numCols = 6;
+const numRows = 6;
+const numCols = 5;
 const outputWidth = 3200; // Half the width of the output image
-const outputHeight = 1800; // Half the height of the output image
+const outputHeight = 2160; // Half the height of the output image
 const skew = 0; // Adjust as needed
 const horizontalShift = 0; // Adjust as needed
 
@@ -115,7 +129,7 @@ async function generateCompositeImage() {
     const background = createBlackBackground(outputWidth, outputHeight);
     console.log("Black background image created successfully.");
 
-    await resizeAndSaveImages(selectedImages, imageWidth, imageHeight, tempDir);
+    await resizeAndSaveImages(selectedImages, imageWidth, tempDir);
     console.log("Selected images resized and saved to the temporary folder.");
 
     const composite = await compositeImages(
