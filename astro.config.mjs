f4c0189080c92a699882b480c4a0fae821b02fa2
Josh Harwood <josh@noworries.nz>
2023-10-18 22:16:32 +1300

    remove pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import pagefind from "astro-pagefind";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://designsystems.media",
-  integrations: [mdx(), sitemap(), pagefind()],
+  integrations: [mdx(), sitemap()],
 });
